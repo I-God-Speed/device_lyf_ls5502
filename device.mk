74079ab8bf73df6167cab3e634e11da99763e7bb
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-07-23 13:38:58 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 #
 
 # Inherit from msm8916-common
-$(call inherit-product, device/lenovo/msm8916-common/msm8916.mk)
+$(call inherit-product, device/lyf/msm8916-common/msm8916.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
